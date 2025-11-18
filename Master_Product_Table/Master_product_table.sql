860b12072f0d6a72d53b605f4dd397b0a0720f03
Prerak Patel <ppatel.nsa@gmail.com>
2025-11-18 11:35:23 -0500

    Refactor parent_descriptions query to filter ITMMAS_DESCR_Bronze by seq_descr range; remove redundant condition.

--- Master_Product_Table/Master_product_table.sql
+++ Master_Product_Table/Master_product_table.sql
@@ -110,11 +110,10 @@ parent_descriptions AS (
         ib.FLAG_STAT_ITEM AS PARENT_ITEM_STATUS,
         LISTAGG(id.descr_addl, '') WITHIN GROUP (ORDER BY SEQ_DESCR) AS "PARENT DESCRIPTION"
     FROM BRONZE_DATA.TCM_BRONZE."ITMMAS_BASE_Bronze" ib
-    LEFT JOIN BRONZE_DATA.TCM_BRONZE."ITMMAS_DESCR_Bronze" id
+    LEFT JOIN (select * from BRONZE_DATA.TCM_BRONZE."ITMMAS_DESCR_Bronze"
+                where seq_descr BETWEEN 800 AND 810) id
            ON ib.id_item = id.id_item
     WHERE ib.code_comm = 'PAR'
-      AND id.seq_descr BETWEEN 800 AND 810
-    --   AND ib."ID_ITEM" ilike '160SG'  -- test filter to limit data volume
     GROUP BY ib.id_item, ib.FLAG_STAT_ITEM
 ),
 
